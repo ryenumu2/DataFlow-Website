cdf8c8b29033e40b8cfe404d09b4b8f71e58e3ec
Rohith Yenumula <42985932+ryenumu2@users.noreply.github.com>
2020-10-07 15:51:38 -0400

    Footer for articles

---
+++ src/components/Footer2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+
+return (
+  <footer2 style={{"textAlign": "center"}}>
+      Developed with <span role="img"> ❤️ </span> by <a class="link-style"  href="https://rohithyen.com" target="_blank">Rohith</a>
+  </footer2>
+  )
+} 
